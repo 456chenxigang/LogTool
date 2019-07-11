dc7d1bdc63be942c4577456a37514bd6cf19a0c7
cxg <1099284882@qq.com>
2019-07-11 16:46:56 +0800

    add something

--- app/src/main/java/dsppa/com/logtool/MyApplication.java
+++ app/src/main/java/dsppa/com/logtool/MyApplication.java
@@ -9,7 +9,7 @@ public class MyApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-
+        //debug
         LogManager.getInstance().init(getApplicationContext());
     }
 }
